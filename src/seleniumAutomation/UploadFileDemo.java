0b9ceb9f6557b9c1a139a8c82be541f41e237a21
shubhs1331 <shubhangigoel1331@gmail.com>
2022-09-08 16:05:22 +0530

    UploadFileDemo added

---
+++ src/seleniumAutomation/UploadFileDemo.java
@@ -0,0 +1,34 @@
+package seleniumAutomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class UploadFileDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		// Launch browser
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/automation-practice-form");
+		
+		//scroll down
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,250)", "");
+		
+		//find upload file web element
+		WebElement uploadFile = driver.findElement(By.id("uploadPicture"));
+		uploadFile.sendKeys("C:\\Users\\Dell\\Documents\\sampleFile.txt");
+		Thread.sleep(3000);
+		System.out.println("File Uploaded Successfully!!!");
+		
+		Actions actions = new Actions(driver);
+		//close browser
+		driver.quit();
+
+	}
+
+}
