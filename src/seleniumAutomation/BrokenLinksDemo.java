5f3217ceea52ca92919c8a38734f476b90764d9e
shubhs1331 <shubhangigoel1331@gmail.com>
2022-09-13 19:52:14 +0530

    BrokenLinksDemo code updated

--- src/seleniumAutomation/BrokenLinksDemo.java
+++ src/seleniumAutomation/BrokenLinksDemo.java
@@ -11,8 +11,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.opentelemetry.exporter.logging.SystemOutLogExporter;
-
 public class BrokenLinksDemo {
 
 	public static void main(String args[]) {
